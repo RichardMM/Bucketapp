b4d4b8f12009f7ce78605c88badad4c76c5b840f
Richard M <rmachaz@gmail.com>
2017-07-26 22:28:23 +0300

    Add more tests
    Create account test included and testing data returned in home page

--- bucketbook app/FlaskProject/test1.py
+++ bucketbook app/FlaskProject/test1.py
@@ -8,25 +8,29 @@ class FlaskBookshelfTests(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True 
 
-
     def test_home(self):
-
         # sends HTTP GET request to the application
         # on the specified path
         result = self.app.get('/') 
         # assert the status code of the response
         self.assertEqual(result.status_code, 200) 
 
-    #------should look at how to make this test work_____
-    #def test_home_data(self):
-    #    # sends HTTP GET request to the application
+    def test_home_data(self):
+        # check create account page is rendered in home route
+        result = self.app.get('/') 
+        # assert the response data
+        self.assertIn(b"Create account to start tracking your dreams", result.data)
 
-    #    # on the specified path
-
-    #    result = self.app.get('/') 
-    #    # assert the response data
-    #    self.assertEqual(result.data, createaccount)
-
+    def test_creataccount(self):
+        #test that creating an account works
+        account_data = {"firstname": "firstname",
+                        "surname": "surname", 
+                        "phone": "phone", 
+                        "emailaddress": "emailaddress", 
+                        "password": "password", 
+                        "pswrepeat": "password"}
+        result = self.app.post("/", data=account_data, follow_redirects=True)
+        self.assertIn(b"email", result.data)
 
 if __name__ == '__main__':
     unittest.main()
